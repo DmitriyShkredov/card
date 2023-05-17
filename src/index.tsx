a5458a8fac4e809e30a2104de9413a0e7e52d1d2
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-05-18 01:53:04 +0300

    feat: add header, theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ReactComponent as IconInstagram } from "./assets/svg/icon_instagram.svg";
-import { ReactComponent as IconTelegram } from "./assets/svg/icon_telegram.svg";
-import "./index.css";
-import "./footer.css";
+import { Main } from "./pages";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <main className="main">
-    <h2 className="subtitle">Hi, I'm</h2>
-    <h1 className="title">Dmitriy Shkredov</h1>
-    <p className="description">Web Developer</p>
-    <footer className="footer">
-      <a
-        href="https://www.instagram.com/regular_web_developer/"
-        className="footer__link"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <IconInstagram />
-      </a>
-      <a
-        href="https://t.me/dmitriy_shkredov"
-        className="footer__link"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <IconTelegram />
-      </a>
-    </footer>
-  </main>
-);
+root.render(<Main />);
---
+++ src/pages/Main.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import { ReactComponent as IconInstagram } from "../assets/svg/icon_instagram.svg";
+import { ReactComponent as IconTelegram } from "../assets/svg/icon_telegram.svg";
+import "./index.css";
+import "./header.css";
+import "./footer.css";
+
+export const Main = () => {
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <main className={classNames("main", theme)}>
+      <header className="header">
+        <button
+          className={classNames("theme-button", theme)}
+          onClick={changeTheme}
+        ></button>
+      </header>
+      <h2 className={classNames("subtitle", theme)}>Hi, I'm</h2>
+      <h1 className={classNames("title", theme)}>Dmitriy Shkredov</h1>
+      <p className={classNames("description", theme)}>Web Developer</p>
+      <footer className="footer">
+        <a
+          href="https://www.instagram.com/regular_web_developer/"
+          className={classNames("footer__link", theme)}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconInstagram />
+        </a>
+        <a
+          href="https://t.me/dmitriy_shkredov"
+          className={classNames("footer__link", theme)}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconTelegram />
+        </a>
+      </footer>
+    </main>
+  );
+};
